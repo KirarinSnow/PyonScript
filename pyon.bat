7b69be549a14909c90d4489a23f2d1a53e192a35
KirarinSnow <kirarinsnow@uchicago.edu>
2010-04-23 05:35:02 -0500

    added pyon.bat script for Windows :(

---
+++ pyon.bat
@@ -0,0 +1,4 @@
+REM        For Windows
+REM        Replace "...gswin32.exe" with path to your copy of Ghostscript.
+
+C:\Program Files\gs\gs8.71\bin\gswin32.exe -q -dNODISPLAY src/library.ps -- src/interpreter.ps %*
--- src/pyon.sh
+++ src/pyon.sh
@@ -6,9 +6,4 @@
 # Run program file
 #   $ pyon file1.pyon file2.pyon ...
 
-if [ $# -eq 0 ]
-then
-    gs -q -dNODISPLAY ${0%/*}/pyon.ps
-else
-    gs -q -dNODISPLAY -- ${0%/*}/pyon.ps $@ </dev/stdin
-fi
+gs -q -dNODISPLAY -- ${0%/*}/pyon.ps $@
